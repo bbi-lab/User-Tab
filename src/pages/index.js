7143c0840a4e9d967bf197463dfdc619103618b6
Lilu Martin <marti400@wwu.edu>
2021-01-07 11:34:42 -0800

    just need a button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
-import React from "react";
-import { Link } from "gatsby";
-import history from "./history";
-import {Switch, BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import styled from 'styled-components';
-import imgUrl from "../pictures/marqueeHome.jpg"
-import '../containers/App.css'
-import { render } from "react-dom";
-import ReactDOM from 'react-dom';
+import React, {useState, useEffect, Fragment} from "react";
+//import Button from 'react-bootstrap/Button'
+import axios from 'axios';
+import button from 'gatsby';
 
-const Button = styled.button`
+const URL_PROJECT = "https://sheets.googleapis.com/v4/spreadsheets/1dzuXRu33dQQWQOpTRQK-9AW9Zk-i69ChRyLAU8l2CxE/values/A3:C?key=AIzaSyAbtv9bc79V3WksFnhqpp0jlcHzlfxopF0";
+const URL_SAMPLE = "https://sheets.googleapis.com/v4/spreadsheets/1dzuXRu33dQQWQOpTRQK-9AW9Zk-i69ChRyLAU8l2CxE/values/Sample%20Tracking!A3:AH?key=AIzaSyAbtv9bc79V3WksFnhqpp0jlcHzlfxopF0";
+
+// 1Snk2U
+/*const Button = styled.button`
 padding: 0.4em;
 margin: 0.5em;
 color: ${props => props.inputColor || "white"};
@@ -18,160 +17,149 @@ border-radius: 4px;
 font: 400 13px Arial;
 min-width: 95px;
 min-height: 10px;
-`;
+`;*/
 
-const Title = styled.p`
-  font-size: 1.75em;
-`;
-
-const http = new XMLHttpRequest();
-const htttp = new XMLHttpRequest();
-var Info1;
-var Info2;
-var projectID = 0;
-var qPCR = "";
-var SeqDate = "";
-var InDate = "";
-var Pasta = "";
-const url1 = "https://sheets.googleapis.com/v4/spreadsheets/1dzuXRu33dQQWQOpTRQK-9AW9Zk-i69ChRyLAU8l2CxE/values/A3:C?key=AIzaSyAbtv9bc79V3WksFnhqpp0jlcHzlfxopF0";
-const url2 = "https://sheets.googleapis.com/v4/spreadsheets/1dzuXRu33dQQWQOpTRQK-9AW9Zk-i69ChRyLAU8l2CxE/values/Sample%20Tracking!A4:AH?key=AIzaSyAbtv9bc79V3WksFnhqpp0jlcHzlfxopF0";
-
-
-function aaaa(ID) {
-  
-  http.open("GET", url1);
-  http.send();
-  http.onload = function () {
-    if (http.status === 200) {
-
-      Info1 = JSON.parse(http.responseText);
-      projectID = 0;
-
-      for (var i = 0; i < Info1.values.length; i++) {
-        if (Info1.values[i][1] === ID) {
-          projectID = Info1.values[i][2];
-          break;
-        }
-      }
-      
-    }
-    htttp.open("GET", url2);
-    htttp.send();
-    
-    htttp.onload = function () {
-      
-    console.log("Status: " + htttp.status);
-      Pasta = "";
-      
-      document.getElementById("Link").innerHTML = "";
-      if (htttp.status === 200) {
-
-        Info2 = JSON.parse(htttp.responseText);
-        console.log("ProjectID = " + projectID);
-        if (projectID == 0) {
-          document.getElementById("Link").innerHTML = " This User ID does not exist ";
-          return;
-        }
-        else {
-          for (var i = 0; i < Info2.values.length; i++) {
-            if (Info2.values[i][0] === projectID) {
-              InDate = Info2.values[i][4] + "";
-              if(InDate === "undefined")
-              {
-                InDate = "N/A";
-              }
-              qPCR = Info2.values[i][29] + "";
-              if(qPCR === "undefined")
-              {
-                qPCR = "N/A";
-              }
-              SeqDate = Info2.values[i][30] + "";
-              if(SeqDate === "undefined")
-              {
-                SeqDate = "N/A";
-              }
-              
-              Pasta += "<u> "+ "<p>" + "Internal ID: " + Info2.values[i][2] +  "|        Intake Date: " + InDate + "|       qPCR pass/fail: " + qPCR + "|       Sequencing Start Date: " + SeqDate + "</p>"  + "</u>";
-            }
-          }
-          document.getElementById("Link").innerHTML = Pasta;
-          
-       
-        }
-      }
-    }
-  }
-  
+const clean = (value) => (value || "").trim();
+const sampleToHtml = (sample) => { 
+  console.log(sample)
+  return (<tr className = "boarder">
+    <td className = "boarder">{sample.projectId}</td>
+    <td className = "boarder">{sample.investigator}</td>
+    <td className = "boarder">{sample.internalId}</td>
+    <td className = "boarder">{sample.investigatorId}</td>
+    <td className = "boarder">{sample.intakeDate}</td>
+    <td className = "boarder">{sample.qPCR}</td>
+    <td className = "boarder">{sample.seqDate}</td>
+  </tr>)
+}
+const errorHtml = (sample) =>{
+  console.log(sample);
+  return(
+    <p>This UserID does not exist</p>
+  )
 }
 
+const Main = () => { 
 
-
-export default function Home() {
-  return (
-    <div  className = "container"  id= "Sauce">
-      <Router history={history}>
-        <Switch>
-          <Route path="/" component={IndexPage} />
-        </Switch>
-      </Router>
-    </div>
-  );
-}
-
-
-
-    class IndexPage extends React.Component {
-      
-      state = {
-        UserID: ""
-
+  const [check, setCheck] = useState(false);
+  const [userID, setUserID] = useState("");
+  const [projects, setProjects] = useState([]);
+  const [myProjects, setMyProjects] = useState([]);
+  
+  // Fires When User Id Changes
+  useEffect(() => { 
+    setMyProjects(projects.filter(v => v.UserId === userID));
+    {
+      if(myProjects.length > 0){
+        setCheck(!check);
       }
-      handleInputChange = event => {
-        const target = event.target
-        const value = target.value
-        const name = target.name
-        this.setState({
-          [name]: value,
-        })
-      }
+    }
+  }, [userID]);
+
+  /*useEffect(() => {
+    clicked(buttonPress => !buttonPress);
+    console.log(buttonPress);
+  }, [])*/
+
+  // Load Data This Only Happens Once
+  useEffect(() => {
+
+    // Load All Data In The Array Below
+    axios.all([
+      axios.get(URL_PROJECT),
+      axios.get(URL_SAMPLE)
+    ]).then(result => { 
+
+      // Function To Clean Up Inputs (Remove White Space + LowerCase)
+      /*
+      */
+      // Turn Arrays Into Objects to Make Them Easier To Reason About
+      const samples = result[1].data.values
+      .map(v => v.map(clean))
+      .map(sample => ({ 
+        projectId: sample[0],
+        investigator: sample[1],
+        internalId: sample[2],
+        investigatorId: sample[3],
+        intakeDate: sample[4],
+        //date: sample[5],
+        qPCR: sample[29],
+        //tissue: sample[7],
+        seqDate: sample[16]
+      }));
+
       
-     handleSubmit = event => {
-        event.preventDefault();
-        aaaa(this.state.UserID);
-        
-      }
-      
-      
-      
-      render() {
-        return (
-          <div>
-          <div >
-          <form>
-          <Title>
-            <label >
-              User ID:
+      // Turn Arrays Into Objects to Make Them Easier To Reason About
+      // Next Samples In Projects To Make Them Easier To Work With
+      const projects = result[0].data.values
+      .filter(v => v.length === 3)
+      .map(v => v.map(clean))
+      .map(project => ({
+        projectId: project[2],
+        investigatorId: project[3],
+        UserId: project[1],
+        samples: samples.filter(sample => (sample.projectId  === project[2])),
+      } ));
+
+     // Save Projects Between Render Calls
+     //console.log(projects);  e=> clicked(buttonPress => !buttonPress)
+     setProjects(projects);
+     
+     
+    });
+  },[]);
+//(myProjects.length > 0)
+  return ( 
+        <Fragment>
+          <div className = "container">
+          <label>
+            User ID:
             <input
-                type="text"
-                name="UserID"
-                value={this.state.UserID}
-                onChange={this.handleInputChange}
-                style={{ width: "400px" }, { height: "20px" }, { margin: "0.5em" }}
-              />
-            </label>
-            <Button onClick = {this.handleSubmit}>Submit</Button>
-          </Title>
-        </form>
+              value={userID}
+              onChange={e => { setUserID("" || e.target.value.trim())}}
+              type="text" />
+              
+          </label>
+          <button onClick = {console.log("wow")}>submit</button>
+          
+      <p/>
+        <table className={(myProjects.length > 0) ? "seeker" : "hider"}>
+          <tbody>
+          <tr className = "boarder">
+            <td className = "boarder">Project ID </td>
+            <td className = "boarder">Investigator </td>
+            <td className = "boarder">Internal ID </td>
+            <td className = "boarder">Investigator ID </td>
+            <td className = "boarder">Intake Date </td>
+            <td className = "boarder">qPCR Results </td>
+            <td className = "boarder">Sequencing Date </td>
+          </tr>
+          {myProjects.map(project => {
+            return(project.samples.map(sample => {return(sampleToHtml(sample))}))
+          }
+          )
+           
+         }
+         
+         </tbody>
+        </table>
         
-        <Link to="/ForgotUser"> Forgot your User ID? </Link>
-        <p></p>
-        </div>
-        <table><tbody id="Link"></tbody></table>
-        </div>
-        )
+      </div>
+    </Fragment>
+        ); 
+  } 
+ //(myProjects.length > 0  <button onClick={setCheck(!check)} >Submit</button>
+ /*const useToggle = (initialState) => {
+    const [isToggled, setIsToggled] = React.useState(initialState);
+  
+    // put [setIsToggled] into the useCallback's dependencies array
+    // this value never changes so the callback is not going to be ever re-created
+    const toggle = React.useCallback(
+      () => setIsToggled(state => !state),
+      [setIsToggled],
+    );
+  
+    return [isToggled, toggle];
   }
-}
-
-
-
-
+  const [isToggled, toggle] = useToggle(false);*/
+export default Main;
